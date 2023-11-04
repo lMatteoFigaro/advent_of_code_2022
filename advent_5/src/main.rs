7c08da2197e41f7cc6a62ffbec42988c31e031a6
Lucas MATTEO <lmatteo@figarocms.fr>
2023-11-04 16:21:46 +0100

    buggy implem

--- advent_5/src/main.rs
+++ advent_5/src/main.rs
@@ -3,9 +3,25 @@ use std::fs::read_to_string;
 
 fn main() {
     let mut stacks = Stacks::new();
+
+    let re = regex::Regex::new("move ([0-9]+) from ([0-9]+) to ([0-9]+)").unwrap();
+
     for line in read_to_string("./input.txt").unwrap().lines() {
-        if line == "" || !line.contains(|c| c == '[' || c == ']') {
-            break;
+        if line == "" {
+            continue;
+        }
+
+        if let Some(caps) = re.captures(line) {
+            let repeat: u8 = caps[1].parse().unwrap();
+            let from: usize = caps[2].parse().unwrap();
+            let to: usize = caps[3].parse().unwrap();
+            println!("got move: {} from : {}, to: {}", repeat, from, to);
+            println!("{:?}", stacks);
+            stacks.move_elem(repeat, from, to);
+        };
+
+        if !line.contains(|c| c == '[' || c == ']') {
+            continue;
         }
 
         line.replace('[', "")
@@ -14,16 +30,14 @@ fn main() {
             .chars()
             .enumerate()
             .for_each(|(i, c)| {
-                print!("{}", c);
                 if c != ' ' {
                     stacks.queue(i / 2, c)
                 }
             });
-
-        println!("");
     }
 
     println!("{:?}", stacks);
+    stacks.print();
 }
 
 #[derive(Debug)]
@@ -42,5 +56,29 @@ impl Stacks {
         }
 
         self.stacks.get_mut(i).unwrap().push_back(val)
+    }
+
+    fn move_elem(&mut self, num: u8, from: usize, to: usize) {
+        let mut i = num;
+        let from = from - 1;
+        let to = to - 1;
+        while i > 0 {
+            let Some(elem) = self
+                .stacks
+                .get_mut(from)
+                .expect("could not get stack")
+                .pop_front()
+            else {
+                return;
+            };
+            self.stacks.get_mut(to).unwrap().push_front(elem);
+            i = i - 1;
+        }
+    }
+
+    fn print(&self) {
+        for e in &self.stacks {
+            print!("{}", e.front().unwrap());
+        }
     }
 }
